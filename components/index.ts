50d56382ed2fe0bcce9cd356fedab4e69eb320a5
nueng <wongsaokrn.kump@gmail.com>
2024-10-24 13:11:09 +0700

    first commit

---
+++ components/index.ts
@@ -0,0 +1,23 @@
+import Hero from "./Hero";
+import CustomButton from "./CustomButton";
+import Navbar from "./Navbar";
+import Footer from "./Footer";
+import CustomFilter from "./CustomFilter";
+import SearchBar from "./SearchBar";
+import SearchManufacuturer from "./SearchManufacturer ";
+import CarCards from "./CarCards";
+import CarDetails from "./CarDetails";
+import ShowMore from "./ShowMore";
+
+export {
+    Hero,
+    CustomButton,
+    Navbar,
+    Footer,
+    CustomFilter,
+    SearchBar,
+    SearchManufacuturer,
+    CarCards,
+    CarDetails,
+    ShowMore
+}
